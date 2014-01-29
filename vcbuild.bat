1319abb3887e3b8d49f4d6b97ded5e32ac5679ba
litmit <litmit@gmail.com>
2014-01-29 09:07:28 +0300

    Allow to use python.bat for building in Windows

--- vcbuild.bat
+++ vcbuild.bat
@@ -89,7 +89,7 @@ if defined NIGHTLY set TAG=nightly-%NIGHTLY%
 @rem Generate the VS project.
 SETLOCAL
   if defined VS100COMNTOOLS call "%VS100COMNTOOLS%\VCVarsQueryRegistry.bat"
-  python configure %debug_arg% %nosnapshot_arg% %noetw_arg% %noperfctr_arg% --dest-cpu=%target_arch% --tag=%TAG%
+  call python configure %debug_arg% %nosnapshot_arg% %noetw_arg% %noperfctr_arg% --dest-cpu=%target_arch% --tag=%TAG%
   if errorlevel 1 goto create-msvs-files-failed
   if not exist node.sln goto create-msvs-files-failed
   echo Project files generated.
@@ -189,7 +189,7 @@ goto exit
 
 :run-tests
 echo running 'python tools/test.py %test_args%'
-python tools/test.py %test_args%
+call python tools/test.py %test_args%
 if "%test%"=="test" goto jslint
 goto exit
 
@@ -211,7 +211,7 @@ goto exit
 :jslint
 echo running jslint
 set PYTHONPATH=tools/closure_linter/
-python tools/closure_linter/closure_linter/gjslint.py --unix_mode --strict --nojsdoc -r lib/ -r src/ --exclude_files lib/punycode.js
+call python tools/closure_linter/closure_linter/gjslint.py --unix_mode --strict --nojsdoc -r lib/ -r src/ --exclude_files lib/punycode.js
 goto exit
 
 :help
@@ -232,6 +232,6 @@ rem ***************
 
 :getnodeversion
 set NODE_VERSION=
-for /F "usebackq tokens=*" %%i in (`python "%~dp0tools\getnodeversion.py"`) do set NODE_VERSION=%%i
+for /F "usebackq tokens=*" %%i in (`call python "%~dp0tools\getnodeversion.py"`) do set NODE_VERSION=%%i
 if not defined NODE_VERSION echo Cannot determine current version of node.js & exit /b 1
 goto :EOF
